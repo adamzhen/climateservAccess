84f41fe06305fb764a6bd6b1c20c36c17037ddd6
adamzhen <adzheng@tamu.edu>
2024-01-10 11:55:52 -0500

    Upload library

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='climateservaccess',
+    version='1.0.0',
+    author='Adam Zheng',
+    author_email='adzheng@tamu.edu',
+    description='A custom library for accessing the ClimateSERV API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/adamzhen/climateservAccess',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'requests'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
